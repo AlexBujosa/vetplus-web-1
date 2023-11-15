650d1f7944960807db8ae92cdff9ffec00feb785
Christopher Tineo <christophertineo02@gmail.com>
2023-11-15 19:31:12 -0400

    try tsx on apollo client

--- src/hooks/use-auth/index.tsx
+++ src/hooks/use-auth/index.tsx
@@ -1,21 +1,21 @@
-import client from "@/utils/apolloClient";
-import { SIGN_WITH_EMAIL_QUERY } from "@/graphql/auth";
-import { useNavigate } from "react-router-dom";
-import useUser from "@/hooks/use-user";
-import { allowedRoles, routes } from "@/config/routes";
-import { useSetAtom } from "jotai";
-import { roleAtom } from "./roleAtom";
+import client from '@/utils/apolloClient'
+import { SIGN_WITH_EMAIL_QUERY } from '@/graphql/auth'
+import { useNavigate } from 'react-router-dom'
+import useUser from '@/hooks/use-user'
+import { allowedRoles, routes } from '@/config/routes'
+import { useSetAtom } from 'jotai'
+import { roleAtom } from './roleAtom'
 
 export type LoginSubmitForm = {
-  email: string;
-  password: string;
-};
+  email: string
+  password: string
+}
 
 export default function useAuth() {
-  const navigate = useNavigate();
-  const { getUserProfile } = useUser();
+  const navigate = useNavigate()
+  const { getUserProfile } = useUser()
 
-  const setRole = useSetAtom(roleAtom);
+  const setRole = useSetAtom(roleAtom)
 
   async function loginWithEmail({ email, password }: LoginSubmitForm) {
     const variables = {
@@ -23,38 +23,37 @@ export default function useAuth() {
         email,
         password,
       },
-    };
+    }
 
     const {
       signInWithEmail,
     }: {
       signInWithEmail: {
-        access_token: string;
-      };
-    } = await client.request(SIGN_WITH_EMAIL_QUERY, variables);
-
-    // const profile = await client.request()
-    const token = signInWithEmail.access_token;
-    localStorage.setItem("token", token);
-    setTimeout(async () => {
-      const profile = await getUserProfile();
-
-      const { role } = profile;
-
-      setRole(role);
-
-      if (!allowedRoles.includes(role)) {
-        throw new Error("Forbidden error");
+        access_token: string
       }
+    } = await client.request(SIGN_WITH_EMAIL_QUERY, variables)
 
-      navigate(routes.admin.pages.clients.href);
-    }, 200);
+    const token = signInWithEmail.access_token
+
+    localStorage.setItem('token', token)
+
+    const profile = await getUserProfile()
+
+    const { role } = profile
+
+    setRole(role)
+
+    if (!allowedRoles.includes(role)) {
+      throw new Error('Forbidden error')
+    }
+
+    navigate(routes.admin.pages.clients.href)
   }
 
   function logout() {
-    localStorage.removeItem("token");
-    navigate(routes.auth.pages.login.href);
+    localStorage.removeItem('token')
+    navigate(routes.auth.pages.login.href)
   }
 
-  return { loginWithEmail, logout };
+  return { loginWithEmail, logout }
 }
