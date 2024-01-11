851b00ccbccff4bebe90949439aa61a87dbdbe9b
Christopher Tineo <christophertineo02@gmail.com>
2024-01-11 09:50:17 -0400

    add appointment observations page

--- src/pages/[admin]/clients/client-appointment-detail/index.tsx
+++ src/pages/[admin]/clients/client-appointment-detail/index.tsx
@@ -1,13 +1,10 @@
-import React, { HTMLProps, PropsWithChildren } from 'react'
 import { Appointment } from '@/types/clinic'
 import { useQueryClient } from '@tanstack/react-query'
 import { useNavigate, useParams } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import { IconButton } from '@mui/material'
 import { KeyboardBackspace } from '@mui/icons-material'
-import { Headline, Label, Title, Body } from '@/components/typography'
-import { useTranslation } from 'react-i18next'
-import cn from '@/utils/cn'
-import { Profile } from '@/components/profile'
+import Image from '@/components/image'
 import dayjs from 'dayjs'
 
 export default function ClientAppointmentPage() {
@@ -30,129 +27,134 @@ export default function ClientAppointmentPage() {
   if (!appointment) return navigate(-1)
 
   return (
-    <section className='flex flex-col mb-10 gap-y-7'>
-      <div className='flex flex-row gap-x-[20px]'>
+    <div className='max-w-4xl mx-auto my-8'>
+      <div className='flex items-center mb-6 space-x-4'>
         <IconButton
           onClick={() => navigate(-1)}
           children={<KeyboardBackspace className='text-black' />}
         />
-        <Headline.Medium text={t('go-back')} />
+        <h1 className='text-lg font-semibold'>Volver atrás</h1>
       </div>
-
-      <div className='space-y-4'>
-        <div className='flex flex-col gap-y-3'>
-          <Title.Large text={t('appointment-details')} />
-          <span className='grid items-center grid-cols-2 grid-rows-1 w-fit gap-x-3'>
-            <Label.Large text={t('pet')} />
-            <Profile
-              image={appointment.Pet.image}
-              profile={appointment.Pet.name}
-            />
-
-            <Label.Large text={t('owner')} />
-            <Profile
-              image={appointment.Owner.image}
-              profile={
-                appointment.Owner.surnames
-                  ? `${appointment.Owner.names} ${appointment.Owner.surnames}`
-                  : appointment.Owner.names
-              }
-            />
-          </span>
-
-          <Label.Large text={t('start-at')} />
-          <Label.Medium text={dayjs(appointment.start_at).format('LLLL')} />
-
-          <Label.Large text={t('end-at')} />
-          <Label.Medium
-            text={
-              appointment.end_at
-                ? dayjs(appointment.end_at).format('LLLL')
-                : 'N/A'
-            }
-          />
+      <div className='p-6 bg-white rounded-lg shadow'>
+        <div className='grid grid-cols-1 gap-6 pb-6 mb-6 border-b md:grid-cols-2'>
+          <div>
+            <h2 className='mb-4 text-xl font-bold'>
+              {t('appointment-details')}
+            </h2>
+            <div className='grid grid-cols-2 gap-4'>
+              <div className='flex items-center gap-x-3'>
+                <span>
+                  <p className='font-semibold'>{t('pet')}</p>
+                  <p>{appointment.Pet.name}</p>
+                </span>
+                <Image
+                  className='w-10 h-10 rounded-full'
+                  src={appointment.Pet.image}
+                />
+              </div>
+              <div className='flex items-center gap-x-3'>
+                <span>
+                  <p className='font-semibold'>{t('owner')}</p>
+                  <p>
+                    {appointment.Owner.surnames
+                      ? `${appointment.Owner.names} ${appointment.Owner.surnames}`
+                      : appointment.Owner.names}
+                  </p>
+                </span>
+                <Image
+                  className='w-10 h-10 rounded-full'
+                  src={appointment.Owner.image}
+                />
+              </div>
+              <div>
+                <p className='font-semibold'>{t('start-at')}</p>
+                <p>{dayjs(appointment.start_at).format('LLLL')}</p>
+              </div>
+              <div>
+                <p className='font-semibold'>{t('end-at')}</p>
+                <p>
+                  {appointment.end_at
+                    ? dayjs(appointment.end_at).format('LLLL')
+                    : 'N/A'}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className='grid grid-cols-2 gap-4'>
+              <div>
+                <p className='font-semibold'>{t('last-deworming')}</p>
+                <p>{t('date')}</p>
+                <p>
+                  {dayjs(appointment.observations.deworming.date).format(
+                    'LLLL'
+                  )}
+                </p>
+              </div>
+              <div>
+                <p className='font-semibold'>{t('product')}</p>
+                <p>{appointment.observations.deworming.product}</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='grid grid-cols-2 gap-4'>
-          <Card className='col-span-2'>
-            <Title.Large text={t('deworming')} />
+        <div className='grid grid-cols-1 gap-6 pb-6 mb-6 border-b md:grid-cols-2'>
+          <div>
+            <h2 className='mb-4 text-xl font-bold'>{t('vaccines')}</h2>
+            <p className='font-semibold'>
+              {t('brand')}: {appointment.observations.vaccines.vaccineBrand}
+            </p>
+            <p>
+              {t('batch')}: {appointment.observations.vaccines.vaccineBatch}
+            </p>
+          </div>
 
-            <CardContent>
-              <div className='grid grid-cols-2 gap-4'>
-                <div>
-                  <Label.Large text={t('date')} />
-                  <Body.Medium text='December 6, 2023' />
-                </div>
-                <div>
-                  <Label.Large text={t('product')} />
-                  <Body.Medium text='ProductoXXX' />
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card>
-            <Title.Large text={t('vaccines')} />
+          {appointment.observations.reproductiveTimeline && (
+            <div className='p-4 border'>
+              <h2 className='mb-2 text-lg font-bold'>
+                {t('reproductive-history')}
+              </h2>
+              {appointment.observations.reproductiveTimeline.dateLastHeat && (
+                <p>
+                  {t('last-heat-date')}:{' '}
+                  {dayjs(
+                    appointment.observations.reproductiveTimeline.dateLastHeat
+                  ).format('LLL')}
+                </p>
+              )}
 
-            <CardContent>
-              <div className='space-y-2'>
-                <div>
-                  <Label.Large text={t('brand')} />
-                  <Body.Medium text='' />
-                </div>
-                <div>
-                  <Label.Large text={t('batch')} />
-                  <Body.Medium text='' />
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card>
-            <Title.Large text={t('reproductive-timeline')} />
-            <CardContent>
-              <div className='space-y-2'>
-                <div>
-                  <Label.Large text={t('reproductive-history')} />
-
-                  <Body.Medium text='' />
-                </div>
-                <div>
-                  <Label.Large text={t('last-heat')} />
-                  <Body.Medium text='' />
-                </div>
-                <div>
-                  <Label.Large text={t('last-birth')} />
-                  <Body.Medium text='' />
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className='col-span-2'>
-            <Title.Large text={t('observations')} />
-            <CardContent>
-              <ul className='pl-5 list-disc'>
-                <li>No current suffering reported</li>
-                <li>No special treatment required</li>
-                <li>Regular feed</li>
-              </ul>
-            </CardContent>
-          </Card>
+              {appointment.observations.reproductiveTimeline.dateLastBirth && (
+                <p>
+                  {t('date-last-birth')}:{' '}
+                  {dayjs(
+                    appointment.observations.reproductiveTimeline.dateLastBirth
+                  ).format('LLL')}
+                </p>
+              )}
+            </div>
+          )}
         </div>
+
+        {appointment.observations.suffering.length > 0 && (
+          <div>
+            <h2 className='mb-4 text-xl font-bold'>{t('suffering')}</h2>
+            <ul className='pl-5 list-disc'>
+              {appointment.observations.suffering.map((suffering) => {
+                return <li key={suffering}>{suffering}</li>
+              })}
+            </ul>
+          </div>
+        )}
+
+        {appointment.observations.treatment && (
+          <div>
+            <h2 className='mb-4 text-xl font-bold'>{t('treatments')}</h2>
+            <ul className='pl-5 list-disc'>
+              {appointment.observations.treatment}
+            </ul>
+          </div>
+        )}
       </div>
-    </section>
+    </div>
   )
-}
-
-interface CardProps extends PropsWithChildren, HTMLProps<HTMLDivElement> {}
-
-function Card(props: CardProps) {
-  return (
-    <article
-      className={cn(props.className, 'border border-base-neutral-gray-800 p-4')}
-    >
-      {props.children}
-    </article>
-  )
-}
-
-function CardContent(props: CardProps) {
-  return <section className={cn(props.className)}>{props.children}</section>
 }
